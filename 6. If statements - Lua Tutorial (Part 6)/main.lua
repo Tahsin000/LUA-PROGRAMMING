975b96025efb47c0e24c94a8d93bf2af90157104
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-03-19 08:38:58 +0600

    update: 19-03-2023

---
+++ 6. If statements - Lua Tutorial (Part 6)/main.lua
@@ -0,0 +1,48 @@
+-- ________ if else __________
+if nil then 
+    print ('statement was true')
+end
+
+
+-- ________ if else ( > ) __________
+local age = 19;
+if age > 17 then
+    print("you may enter");
+end
+
+
+-- ________ if else __________
+-- true and true => execute 
+-- true and false => not
+-- false and true => not
+-- false => not
+-- true or true => execute
+-- rue or false => execute
+-- false or true => execute
+-- false or false => not www
+
+
+-- ________ if else ( == ) __________
+local name = "tom";
+if name == "tom" then
+    print("You may Enter");
+end
+
+
+-- ________ if else ( && ) __________
+local name = "tom";
+if name == "tom" and age < 22 then
+    print("You may Enter");
+end
+
+
+
+
+
+
+
+
+
+
+
+
